7e82a8105cc3001b2c6f237949abdcc82371c858
sveta.k55555@gmail.com <sveta.k55555@gmail.com>
2024-05-24 01:39:40 +0300

    commit

---
+++ src/config.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb+srv://sveta:Sveta55@portal.loltykn.mongodb.net/");
+
+connect.then(() => {
+    console.log("Database connected successfully");
+})
+.catch(() => {
+    console.log("Database cannot be connected");
+});
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+const collection = new mongoose.model("users", LoginSchema);
+
+module.exports = collection;
